8035d39080e5b7e4e6fb9df33556645966521b19
Dhinnuh <dhineshcruise@gmail.com>
2024-02-05 14:52:54 +0530

    Feat: Deployed

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATTIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.pth.join(BASE_DIR, 'staticfiles')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
